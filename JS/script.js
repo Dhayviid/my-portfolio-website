43524053e7ae6c230b8f5ded30b56814bef399e4
David King <phoenixdkd2004@gmail.com>
2023-07-03 20:31:13 +0100

    latest changes

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+/*    ========= Typing Animation=============   */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphics Designer","Chef"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
